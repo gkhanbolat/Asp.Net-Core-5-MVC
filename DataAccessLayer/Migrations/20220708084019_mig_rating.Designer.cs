f7017aa9c784f15aa3c5477e06baf94d42a391d9
Gökhann <gkhan35@hotmail.com>
2022-07-08 11:45:35 +0300

    commit2

--- DataAccessLayer/Migrations/20220708084019_mig_rating.Designer.cs
+++ DataAccessLayer/Migrations/20220708084019_mig_rating.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220708084019_mig_rating")]
+    partial class mig_rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
